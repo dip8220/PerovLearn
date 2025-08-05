9ca7fad86fb0f1113a2dfa75e79a53fc72780eb5
dipu-1993 <dipayan.chatterjee.21@aot.edu.in>
2025-08-05 20:24:29 +0530

    LinearModels.py

--- LinearModels.py
+++ LinearModels.py
@@ -10,7 +10,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 from scipy.stats import pearsonr, spearmanr
 
 # Load dataset
-data = pd.read_csv(r'D:\Final Year Project\Independent_expanded_with_Actual_Bandgap.csv')
+data = pd.read_csv(r'D:\Final Year Project\Independent_expanded.csv')
 X = data[['MA', 'FA', 'Cs', 'Cl', 'Br', 'I', 'Pb', 'Sn']]
 y = data['Bandgap']
 
@@ -94,3 +94,4 @@ sns.heatmap(performance_df, annot=True, cmap='YlOrBr', fmt='.6f', linewidth=.5, 
 plt.tight_layout()
 plt.savefig('linear_models_performance_heatmap.tiff', format='tiff', dpi=1200)
 plt.show()
+

37f11439b5e98eebb916c8d2990156cace8a5f09
dipu-1993 <dipayan.chatterjee.21@aot.edu.in>
2025-08-05 20:20:43 +0530

    Comparison6ML.py

--- Comparison6ML.py
+++ Comparison6ML.py
@@ -16,7 +16,7 @@ from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras import layers
 
 # Load dataset
-data = pd.read_csv('D:/Final Year Project/Independent_expanded_with_Actual_Bandgap.csv')
+data = pd.read_csv('D:/Final Year Project/Independent_expanded.csv')
 X = data[['MA', 'FA', 'Cs', 'Cl', 'Br', 'I', 'Pb', 'Sn']]
 y = data['Bandgap']
 
@@ -134,3 +134,4 @@ sns.heatmap(performance_df, annot=True, cmap='YlOrBr', fmt='.6f', linewidth=.5, 
 plt.title('Model Performance Comparison')
 plt.savefig('Model_Performance_Comparison_Heatmap.tiff', format='tiff', dpi=1200)
 plt.show()
+
